1fcddb3c685711613dbc4da34192c9a98d2c41a6
Anwesh43 <anweshthecool0@gmail.com>
2018-01-14 01:36:52 +0530

    adding State

--- imageclipscaleview/src/main/java/ui/anwesome/com/imageclipscaleview/ImageClipScaleView.kt
+++ imageclipscaleview/src/main/java/ui/anwesome/com/imageclipscaleview/ImageClipScaleView.kt
@@ -46,4 +46,21 @@ class ImageClipScaleView(ctx:Context,var bitmap:Bitmap):View(ctx) {
             canvas.drawRect(RectF(x,y,x+1,y+1),paint)
         }
     }
+    data class State(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1-2*scale
+                startcb()
+            }
+        }
+    }
 }
